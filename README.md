ce597f3b808e55cfd91a832087b6d3c8acf2833b
Rik Starmans <info@hexastorm.com>
2020-01-06 12:43:27 +0100

    added plot with dft

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ The LIS3DH has higher sampling rates.
 
 <p hidden>
 Test;
- 1. How many samples can you acquire per second
+ 1. How many samples can you acquire per second  --> 970 measurements
  2. Timestamp and acquire your data
       - what is the max min value if your polygon is on
       - what is the max min value if your polygon is off
      --> can you detect that the polygon is running
  3. Plot your timestamped data, can you see an imbalance?
+      --> yes you can see an imbalance in the discrete fourier transform
+          you see several peaks at overtones of 100 hertz.
+    The DFFT requires you to sample at constant time stamps
+ 4. Try to determine the phase lag via the cross correlation
 <p>
--- plotresults.py
+++ plotresults.py
@@ -1,25 +1,35 @@
 import pickle
 import matplotlib.pyplot as plt
+import scipy.fftpack
+import numpy as np
 
 
 def readplotdata():
     dct = pickle.load(open('results.p', 'rb'))
     for key, value in dct.items():
         print(key)
+    # values = dct['values']
+    # x_accel = values[0]
+    # print(x_accel[1:30])
 
 def plotdata():
     dct = pickle.load(open('results.p', 'rb'))
     tpassed = dct['tpassed']
+    print("RPS is {}".format(dct['rps']))
     values = dct['values']
-    x_accel = values[0]
-    rps = dct['rps']
-    revfraction = []
-    for i in range(len(x_accel)):
-        revolution = i/len(x_accel)*tpassed*rps
-        if revolution < 3:
-            revfract = revolution%1
-            revfraction.append(revfract)
-    plt.plot(revfraction, x_accel[0:len(revfraction)], 'ro')
+    x_res = values[0]
+    x_accel = [x_res[i] for i in range(len(x_res)-1) if x_res[i]!=x_res[i+1]]
+    # time plot
+    # time = [t*(tpassed/len(x_accel)) for t in range(len(x_accel))]
+    # plt.plot(time, x_accel, 'ro')
+    # plt.show()
+    # discrete fourier transform
+    yf = scipy.fftpack.fft(x_accel)
+    T = tpassed/len(x_accel)
+    N = len(x_accel)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
+    #plt.plot(xf, 2.0/N*np.abs(yf[:N//2]))
+    plt.plot(xf, 2.0/N*np.angle(yf[:N//2]))
     plt.show()
 
 

e861406cdacfe3ac699a45777695fbd150c4f9e5
Rik Starmans <info@hexastorm.com>
2020-01-10 16:28:48 +0100

    did succesfull test with accelerometer
    determined cross correlation with artificial signal.
    was able to determine the RPM with a photo tachometer

--- README.md
+++ README.md
@@ -3,15 +3,16 @@
 The goal of this project is to make a balancer for rotors.
 
 # Method
-Vibrations caused by a spinning rotor are measured with an accelerometer. 
+Details with pictures cdan be found on [Hackadday](https://hackaday.io/project/21933-open-hardware-fast-high-resolution-laser/log/172827-rotor-stabilization-experiments).
+Vibrations caused by a spinning rotor are measured with an accelerometer.
 I did an experiment where I spun the rotor at 100 Hertz and recorded the vibrations for 1 second at 800 Hertz sampling frequency.
 Sampling of the signal must be equidistant or otherwise the discrete fourier transform can not be calculated.
 The amplitude of the discrete fourier transform at a 100 hertz is used to determine the balance weight.
 This amplitude can be linearly compared to the height of the amplitude determined from a known balance weight.
-As the [centrepetal force](https://en.wikipedia.org/wiki/Centripetal_force) is linear proportional to mass.
+As the [centripetal force](https://en.wikipedia.org/wiki/Centripetal_force) is linear proportional to mass.
 A practical example is given by a case study in [Science direct](https://www.sciencedirect.com/science/article/pii/S2351988616300185).
 Note, above steps are only sufficient for single plane balancing. In two plane balancing the procedure is more [complicated](https://forums.ni.com/t5/Example-Programs/Two-Plane-Balancing-Example-with-DAQmx/ta-p/3996066?profile.language=en).
-To determine the position of the mass, the position of the rotor must be measured. For examply by using a [photo tachometer](https://hackaday.com/2017/03/17/how-to-use-a-photo-tachometer/). The phase-difference of the periodic signal recorded with the laser sensor can be compared with the periodic signal of the acceloremeter. The cross correction of the two signals can be determined and from it the phase difference, an example in Python is given [here](https://stackoverflow.com/questions/6157791/find-phase-difference-between-two-inharmonic-waves).
+To determine the position of the mass, the position of the rotor must be measured. For example by using a [photo tachometer](https://hackaday.com/2017/03/17/how-to-use-a-photo-tachometer/). The phase-difference of the periodic signal recorded with the laser sensor can be compared with the periodic signal of the acceloremeter. The cross correction of the two signals can be determined and from it the phase difference, an example in Python is given [here](https://stackoverflow.com/questions/6157791/find-phase-difference-between-two-inharmonic-waves).
 Other methods of determining the phase difference between two signals are listed [here](https://stackoverflow.com/questions/27545171/identifying-phase-shift-between-signals). In the open-hardware laser scanner, the position of the rotor can be determined by recording the number of facets counted with the photo diode.
 The challenge, however, is that the rotor is not fixed. My first idea was to measure the rotor with
 a photo camera having a global shutter. My current idea is a laser tachometer and a white glass marker.
--- plotresults.py
+++ plotresults.py
@@ -9,7 +9,7 @@ def readplotdata():
     '''
     displays keys of dictionary
     '''
-    dct = pickle.load(open('results.p', 'rb'))
+    dct = pickle.load(open('20hertzprism.p', 'rb'))
     for key, value in dct.items():
         print(key)
     # values = dct['values']
@@ -22,52 +22,67 @@ def plotdata():
     plots data in time
     calculates discrete fourier transform plots amplitude or phase
     '''
-    dct = pickle.load(open('results.p', 'rb'))
+    dct = pickle.load(open('20hertzmirror.p', 'rb'))
     tpassed = dct['tpassed']
     print("RPS is {}".format(dct['rps']))
     values = dct['values']
-    x_res = values[0]
+    x_res = values[1]
+    print(x_res)
     x_accel = [x_res[i] for i in range(len(x_res)-1) if x_res[i]!=x_res[i+1]]
     # time plot
     # time = [t*(tpassed/len(x_accel)) for t in range(len(x_accel))]
     # plt.plot(time, x_accel, 'ro')
     # plt.show()
     # discrete fourier transform
+    print(x_accel)
     yf = scipy.fftpack.fft(x_accel)
     T = tpassed/len(x_accel)  # THIS IS NOT CORRECT, sensor is 800 hertz
     N = len(x_accel)
     xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-    #plt.plot(xf, 2.0/N*np.abs(yf[:N//2]))
-    plt.plot(xf, 2.0/N*np.angle(yf[:N//2]))
+    fig, ax = plt.subplots()
+    ax.plot(xf, 2.0/N*np.abs(yf[:N//2]))
+    ax.set(xlabel='frequency (Hz)', ylabel='ticks (1000 ticks is g)')
+    ax.grid()
+    fig.savefig("20hertzmirror.png")
+    # plt.plot(xf, 2.0/N*np.angle(yf[:N//2]))
     plt.show()
 
 
 def crosscorrelate():
     '''
     function to test the difference in phase between the accelerometer 
-    signal and photo tachomater, fake signal is used at the moment
+    signal and photo tachomater, fake signal is used at the moment\
+     --> see https://stackoverflow.com/questions/6157791/find-phase-difference-between-two-inharmonic-waves
     '''
-    dct = pickle.load(open('results.p', 'rb'))
+    dct = pickle.load(open('20hertzprism.p', 'rb'))
     tpassed = dct['tpassed']
     values = dct['values']
-    result = values[0]
-    freq = 100 # hertz, simulated
+    # reguralize datasets
+    result = np.array(values[0])
+    result -= result.mean()
+    result /= result.std()
+    # parameters used for artificial signal
+    freq = 96 # hertz, simulated
     phasediff = 0
+    #timeshift = 0
+    # --recovered phae shift 0.09
     N = len(result)
     T = tpassed/N  # THIS IS NOT CORRECT, sensor is 800 hertz
     x = np.linspace(0.0, N*T, N)
+    # quick test with artificial signal
     artificial = np.sin(freq*2*np.pi*x+phasediff)
+    #artificial = np.roll(artificial, timeshift)
+    # quick test with original signal
+    #artificial = np.roll(result, timeshift)
     # calculate cross correlation of the two signal
-    xcorr = correlate(result, artificial)
-    # The peak of the cross-correlation gives the shift between the two signals
-    # The xcorr array goes from -nsamples to nsamples
-    dt = np.linspace(-x[-1], x[-1], 2*N-1)
+    xcorr = correlate(artificial, result)
+    # delta time array to match xcorrr
+    dt = np.arange(1-N, N)
     recovered_time_shift = dt[xcorr.argmax()]
-    # # force the phase shift to be in [-pi:pi]
-    # recovered_phase_shift = 2*np.pi*(((0.5 + recovered_time_shift/period) % 1.0) - 0.5)
-    print(recovered_time_shift)
-    # see https://stackoverflow.com/questions/6157791/find-phase-difference-between-two-inharmonic-waves
-    # you need a phase shift as you require degrees, see post
+    # force the phase shift to be in [-pi:pi]
+    recovered_phase_shift = 2*np.pi*(((0.5 + recovered_time_shift/T) % 1.0) - 0.5)
+    print("Recovered time shift {}".format(recovered_time_shift))
+    print("Recovered phase shift {}".format(recovered_phase_shift))
+    
 
-
-crosscorrelate()
+plotdata()
